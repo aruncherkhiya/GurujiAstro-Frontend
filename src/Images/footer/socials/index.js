029d883f29a424de24fee3a52f97c644c06eb470
Arun Kumar <102018578+aruncherkhiya@users.noreply.github.com>
2023-06-03 10:52:42 +0530

    Create index.js

---
+++ src/Images/footer/socials/index.js
@@ -0,0 +1,17 @@
+import facebook from './facebook.png';
+import instaI from './instainner.png';
+import instaO from './instaouter.png';
+import twitter from './twitter.png';
+import logoBg from './logobg.png';
+import yt from './yt.avif';
+
+const socialLogos = {
+    facebook,
+    instaI,
+    instaO,
+    twitter,
+    logoBg,
+    yt
+}
+
+export default socialLogos;
