c94b38d218bdb250d01856fdb4b563cae79e9b4a
Arun Kumar <102018578+aruncherkhiya@users.noreply.github.com>
2023-06-03 10:41:42 +0530

    Add files via upload

---
+++ src/component/index.js
@@ -0,0 +1,6 @@
+export { default as Header} from './header/Header'
+export { default as Footer} from './footer/Footer'
+export { default as VideoCarsoul } from './videocal/VideoCarsoul'
+export { default as Astrologers } from './astrologers/Astrologers'
+export { default as ManyPOS } from './manyPOS/ManyPOS'
+export { default as Review } from './review/Review'
