88e01385c734bffc0f66c459d7852972fdaedab4
Arun Kumar <102018578+aruncherkhiya@users.noreply.github.com>
2023-06-03 11:07:33 +0530

    Add files via upload

---
+++ src/Images/index.js
@@ -0,0 +1,15 @@
+import gurujiLogo from './gurujiLogo.png'; 
+import endRef from './endRef.png';
+import star from './star.png'
+import review from './review.png'
+import userbottom from './userbottom.png'
+
+const commonImg = {
+    gurujiLogo,
+    endRef,
+    star,
+    review,
+    userbottom,
+}
+
+export default commonImg;
