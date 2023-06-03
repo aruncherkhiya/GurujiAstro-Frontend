5d2384e21f37c4389b2490e1a9b65a5487e887ae
Arun Kumar <102018578+aruncherkhiya@users.noreply.github.com>
2023-06-03 10:38:39 +0530

    Create data.js

---
+++ src/component/manyPOS/data.js
@@ -0,0 +1,33 @@
+import left1 from '../../Images/animation logos/left1.png';
+import left2 from '../../Images/animation logos/left2.png';
+import left3 from '../../Images/animation logos/left1.png';
+import left4 from '../../Images/animation logos/left4.png';
+import left5 from '../../Images/animation logos/left5.png';
+import right1 from '../../Images/animation logos/right1.png';
+import right2 from '../../Images/animation logos/right2.png';
+import right3 from '../../Images/animation logos/right3.png';
+import right4 from '../../Images/animation logos/right4.png';
+import right5 from '../../Images/animation logos/right5.png';
+
+const left = [
+    left1,
+    left2,
+    left3,
+    left4,
+    left5
+]
+
+const right = [
+    right1,
+    right2,
+    right3,
+    right4,
+    right5
+]
+
+const data = {
+    left,
+    right
+}
+
+export default data;
