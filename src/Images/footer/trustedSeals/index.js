d7ba312859873553e0ec3fb1aefd057f10d328f7
Arun Kumar <102018578+aruncherkhiya@users.noreply.github.com>
2023-06-03 10:54:45 +0530

    Create index.js

---
+++ src/Images/footer/trustedSeals/index.js
@@ -0,0 +1,13 @@
+import payBg from './background.png';
+import paytm from './paytm.png';
+import razorpay from './razorpay.png';
+import stripe from './stripe.png';
+
+const pay = {
+    payBg,
+    paytm,
+    razorpay,
+    stripe
+}
+
+export default pay;
